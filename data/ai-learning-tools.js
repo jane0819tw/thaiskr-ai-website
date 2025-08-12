00a83ad1e0afddc0f4e175bee874afdd6499cf98
Jane Kuo <jane0819tw@gmail.com>
2025-08-12 12:03:11 +0800

    add ai-keyboard page

--- data/ai-learning-tools.js
+++ data/ai-learning-tools.js
@@ -2,6 +2,6 @@ export const learningTools = [
             {
                 name: "泰語發音鍵盤",
                 imageUrl: "https://placehold.co/600x400/43857A/FFFFFF?text=泰語字母閃卡",
-                link: "#" // 請替換成您的應用程式連結
+                link: "./ai-thai-keyboard.html" // 請替換成您的應用程式連結
             }
         ];
