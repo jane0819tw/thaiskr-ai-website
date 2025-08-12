2614c1e43d484e0e2affef9e35578250c57fd612
Jane Kuo <jane0819tw@gmail.com>
2025-08-12 13:22:24 +0800

    fix link image again

--- data/ai-learning-tools.js
+++ data/ai-learning-tools.js
@@ -1,7 +1,7 @@
 export const learningTools = [
             {
                 name: "泰語發音鍵盤",
-                imageUrl: "./img/ai-thai-keyboard.png",
+                imageUrl: "img/ai-thai-keyboard.png",
                 link: "./ai-thai-keyboard.html" // 請替換成您的應用程式連結
             }
         ];
