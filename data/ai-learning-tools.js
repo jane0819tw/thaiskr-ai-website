06f09163a64d5fac245a7fe6fbb4e1343116ffc3
Jane Kuo <jane0819tw@gmail.com>
2025-08-18 21:21:57 +0800

    add ai-daily word

--- data/ai-learning-tools.js
+++ data/ai-learning-tools.js
@@ -3,5 +3,10 @@ export const learningTools = [
                 name: "泰語發音鍵盤",
                 imageUrl: "img/ai-thai-keyboard.png",
                 link: "./ai-thai-keyboard.html" // 請替換成您的應用程式連結
+            },
+             {
+                name: "泰語每日一字",
+                imageUrl: "img/ai-daily-word.png",
+                link: "./ai-daily-word.html" // 請替換成您的應用程式連結
             }
         ];
